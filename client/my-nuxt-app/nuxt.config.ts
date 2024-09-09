dd6d9112df1b4e2b843008e4c46c6ec710911127
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-09-09 22:50:26 +0900

    add: Firebase Auth settings

--- client/my-nuxt-app/nuxt.config.ts
+++ client/my-nuxt-app/nuxt.config.ts
@@ -2,5 +2,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
+    '@pinia/nuxt',
+  ],
+  plugins: [
+    '~/plugins/firebase.ts'
   ],
 })

c7deac7acce18df5e7bfb97b33cc0f0113aec226
ぱらぽねら <138425064+Isshinfunada@users.noreply.github.com>
2024-09-09 23:09:53 +0900

    change: login apiをfirebase使った実装に変更

--- server/main.go
+++ server/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 
 	"github.com/Isshinfunada/TodoList/server/config"
 	"github.com/Isshinfunada/TodoList/server/models"
@@ -20,13 +24,34 @@ func initDB(cfg *config.Config) (*pgxpool.Pool, error) {
 	return dbpool, nil
 }
 
+// initFirebaseは、Firebase Admin SDKを初期化します。
+func initFirebase() (*firebase.App, error) {
+	opt := option.WithCredentialsFile("firebase-adminsdk.json") // この行を適切なパスに変更してください
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, err
+	}
+	return app, nil
+}
+
 // startServerは、Echoサーバーを起動します。
-func startServer(dbpool *pgxpool.Pool) {
-	// log.Println("Server is starting...")
+func startServer(dbpool *pgxpool.Pool, firebaseApp *firebase.App) {
 	e := echo.New()
 	queries := models.New(dbpool)
-	routes.InitRoutes(e, queries)
-	e.Logger.Fatal(e.Start(":8080"))
+
+	// Firebase Auth クライアントを初期化
+	authClient, err := firebaseApp.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("Error getting Auth client: %v\n", err)
+	}
+
+	routes.InitRoutes(e, queries, authClient)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
 func main() {
@@ -43,6 +68,12 @@ func main() {
 	}
 	defer dbpool.Close()
 
+	// Firebase Admin SDKを初期化します。
+	firebaseApp, err := initFirebase()
+	if err != nil {
+		log.Fatalf("Could not initialize Firebase: %v", err)
+	}
+
 	// サーバーを起動します。
-	startServer(dbpool)
+	startServer(dbpool, firebaseApp)
 }
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -5,12 +5,13 @@ import (
 	"github.com/Isshinfunada/TodoList/server/models"
 	"github.com/Isshinfunada/TodoList/server/services"
 
+	"firebase.google.com/go/auth"
 	echojwt "github.com/labstack/echo-jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func InitRoutes(e *echo.Echo, db *models.Queries) {
+func InitRoutes(e *echo.Echo, db *models.Queries, authClient *auth.Client) {
 	// CORS設定を追加
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000"},
@@ -20,7 +21,7 @@ func InitRoutes(e *echo.Echo, db *models.Queries) {
 
 	// 認証が不要なルート
 	e.POST("/users", handlers.RegisterUser(db))
-	e.POST("/login", handlers.Login(db))
+	e.POST("/login", handlers.Login(authClient))
 
 	// 認証が必要なルート
 	authenticated := e.Group("/todos")
